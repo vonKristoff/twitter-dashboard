aa792720d41c4a2c3ea0da87b58ac2de8cef421f
Vonkristoff <mrjcnicolas@gmail.com>
2015-03-01 11:02:38 +0000

    index page

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include("authentication.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Twitter Dashboard</title>
+	<link type="text/css" href="style.css" rel="stylesheet" media="all" />
+</head>
+<body>
+
+</body>
+
+<script data-main="js/main" src="bower_components/requirejs/require.js"></script>
+</html>
+<?php include("callback.php"); ?>
